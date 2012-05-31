c6fcf8e31f70d7f38d0615b3d5d23b5336b88346
Davie Moston <daviemoston@gmail.com>
2012-05-31 18:27:27 +0100

    fix dependencies to allow clojure 1.4

--- project.clj
+++ project.clj
@@ -2,16 +2,13 @@
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [clj-http "0.2.7"]
-                ;; [ring/ring.core "1.0.2"]
                  [ring/ring-core "1.0.2"]
                  [ring/ring-jetty-adapter "1.0.2"]
                  [ring/ring-devel "1.0.2"]
                  [ring-reload-modified "0.1.1"]
-                 [compojure "1.0.1"]
                  [hiccup "0.3.8"]
-                 [noir "1.2.2"]
+                 [noir "1.2.2" :exclusions [org.clojure/clojure]]
                  ]
   :dev-dependencies [[lein-run "1.0.1-SNAPSHOT" ]]
   :main prod-monitor.core
-  
   )
